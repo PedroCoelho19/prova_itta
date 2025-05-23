636f63f26ff5fcff0b6667abc6e3a3df83e3ab13
Wladson14 <wladsonjacinto@gmail.com>
2025-05-23 11:29:07 -0300

    Wladson

---
+++ ex2.js
@@ -0,0 +1,13 @@
+function calcular() {
+    let num = document.getElementById('#num')
+    
+     if (num % 2 === 0) {
+        alert('PAR')
+    } else if (num % 2 === 1 ) {
+        alert('ÍMPAR')
+    }
+
+
+
+ }
+
