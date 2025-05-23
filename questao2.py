4645b5a142e95f1c7261ea53ce5ce5144ba314fe
mauriciooliveiradacosta <maumau.olliver@gmail.com>
2025-05-23 11:47:56 -0300

    prova

--- questao2.py
+++
@@ -1,5 +0,0 @@
-numero= int(input("digite um numero"))
-if numero%2 == 0:
-    print(f"{numero} é par")
-else: 
-    print(f"{numero} é impar")
--- questao3.py
+++
@@ -1,3 +0,0 @@
-numero=[5,1,8,2,3]
-if numero[0]>numero[1]:
-    
